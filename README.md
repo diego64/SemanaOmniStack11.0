99cb9510282c5f2d967d675d9f522f64ed2417c3
diegoferreira <diegoferreira1964@gmail.com>
2020-03-29 17:42:02 -0300

    Projeto Completo

---
+++ README.md
@@ -0,0 +1,41 @@
+<h4 align="center">
+ 👍 SemanaOmniStack11.0
+</h4>
+
+<p align="center">
+  <a href="#rocket-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Desenvolvimento</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Funcionalidades adcionandas no back-end</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Referência</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+</p>
+
+<br>
+
+## 📡 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- [Node.js](https://nodejs.org/en/)
+- [React](https://reactjs.org/)
+- [React Native](https://reactnative.dev/)
+- [Expo](https://expo.io/)
+
+## 📚 Projeto
+
+Sistema foi desenvolvido para cadastrar projetos de ações de ajuda a pessoas publincando um valor de auxio financeiro. Um usuário poderá enviar um e-mail ou WhatsApp informando sua ajuda a um determinado projeto.
+
+## 🔋 Funcionalidades adcionandas no back-end                            
+
+- Validação de dados no cadastro 
+- Adição de novos teste para verificação do código
+- Correção de bug no em um dos teste 
+
+## 🔧 Funcionalidades adcionandas no front-end
+
+- Preenchimento obrigatórios nos campos de cadastro
+- Padronização do modelo de celular
+
+## 📹 Referência 
+
+- Semana OmniStack 11.0 
---
+++ backend/src/routes.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+
+//Validação de campos para cadastro e a declarção da rota
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), OngController.create);
+
+//Validação de campos para listagem de casos cadastrados de uma ONG e a declarção da rota
+routes.get('/profile', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+    }).unknown(),
+}), ProfileController.index);
+
+//Validação da Paginação
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number(),
+    })
+}),IncidentController.index); 
+
+//Validação de campos para criar um caso
+routes.post('/incidents', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        value: Joi.number().required(),
+    }),
+}), IncidentController.create);  
+
+//Validação para apagar um caso somente pelo seu ID
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id:Joi.number().required(),
+    })
+}),IncidentController.delete);
+
+module.exports = routes;
